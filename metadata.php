ed55f39ea0cec29e8cc27447b8151acd840441e1
vanilla thunder <vanilla-thunder@github.com>
2016-03-01 23:15:26 +0100

    initial commit

---
+++ metadata.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ *	bestlife AG - static brutto price no matter of VAT
+ *  Copyright (C) 2016  bestlife AG
+ *  info:  oxid@bestlife.ag
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+$sMetadataVersion = '1.1';
+$aModule = array(
+	'id' => 'bla-staticbrutto',
+	'title' => '<strong style="color:#95b900;font-size:125%;">best</strong><strong style="color:#c4ca77;font-size:125%;">life</strong> <strong>static brutto price</strong>',
+	'description' => 'Adds support for countryspcefic VAT and calculates and keeps same brutto price among different VAT percentage',
+	'thumbnail' => 'bestlife.png',
+	'version' => '1.0.0 (2015-03-15)',
+	'author' => 'Marat Bedoev, bestlife AG',
+	'email' => 'oxid@bestlife.ag',
+	'url' => 'http://www.bestlife.ag',
+	'extend' => array(
+		'oxvatselector' => 'bla/bla-staticbrutto/extend/oxvatselector_vat'
+	),
+	'files' => array(
+		'bla_vat' => 'bla/bla-staticbrutto/files/bla_vat.php',
+		'bla_vat_events' => 'bla/bla-staticbrutto/files/bla_vat_events.php',
+	),
+	'templates' => array(
+		'bla_vat.tpl' => 'bla/bla-staticbrutto/views/bla_vat.tpl'
+	),
+	'events' => array(
+		'onActivate' => 'bla_vat_events::setup',
+	),
+	'settings' => array(
+		array('group' => 'blaVatMain', 'name' => 'aaBlaFullVat', 'type' => 'aarr', 'value' => ''),
+		array('group' => 'blaVatMain', 'name' => 'aaBlaReducedVat', 'type' => 'aarr', 'value' => ''),
+	)
+);
---
+++ views/bla_vat.tpl
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+</head>
+<body>
+<form name="transfer" id="transfer" action="[{ $oViewConf->getSelfLink() }]" method="post">
+	[{ $oViewConf->getHiddenSid() }]
+	<input type="hidden" name="oxid" value="[{ $oxid }]">
+	<input type="hidden" name="oxidCopy" value="[{ $oxid }]">
+	<input type="hidden" name="cl" value="country_main">
+	<input type="hidden" name="language" value="[{ $actlang }]">
+</form>
+<form id="vats" method="post" action="[{$oViewConf->getSelfLink()}]">
+	[{ $oViewConf->getHiddenSid() }]
+	<input type="hidden" name="cl" value="bla_vat"/>
+	<input type="hidden" name="fnc" value="save_vats"/>
+	<table style="width:500px;margin: 0 auto;" border="0" cellspacing="10" cellpadding="0">
+		<colgroup><col width="50%"><col width="25%"><col width="25%"></colgroup>
+		<tr>
+			<td><strong>[{oxmultilang ident="bla_vat_country"}]</strong></td>
+			<td><strong>[{oxmultilang ident="bla_vat_first"}]</strong></td>
+			<td><strong>[{oxmultilang ident="bla_vat_second"}]</strong></td>
+		</tr>
+		[{foreach from=$countries item="country" name="countries"}]
+			[{assign var="id" value=$country->oxcountry__oxid->value }]
+			<tr>
+				<td><label>[{$country->oxcountry__oxtitle->value}]</label></td>
+				<td><input type="text" name="aaBlaFullVat[[{$country->oxcountry__oxid->value}]]"  value="[{ $firstvats.$id }]"  size="3"/>%</td>
+				<td><input type="text" name="aaBlaReducedVat[[{$country->oxcountry__oxid->value}]]" value="[{ $secondvats.$id }]" size="3"/>%</td>
+			</tr>
+		[{/foreach}]
+		<tr>
+			<th colspan="3">
+				[{if $msg}]<h2 style="color:green">[{$msg}]</h2>[{/if}]<button type="submit">[{oxmultilang ident="bla_vat_save"}]</button>
+			</th>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
